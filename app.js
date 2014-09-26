4a98a8e9ef380077945f48d6ad5d0bb8cf3ef1b2
Marcelo Berta <marcelo.berta@gmail.com>
2014-09-26 06:47:12 -0400

    updated app

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ $(document).ready( function() {
 	});
 });
 
-// this function takes the question object returned by StackOverflow 
+// this function takes the question object returned by StackOverflow
 // and creates new result to be appended to DOM
 var showQuestion = function(question) {
-	
+
 	// clone our result template code
 	var result = $('.templates .question').clone();
-	
+
 	// Set the question properties in result
 	var questionElem = result.find('.question-text a');
 	questionElem.attr('href', question.link);
@@ -31,21 +31,40 @@ var showQuestion = function(question) {
 
 	// set some properties related to asker
 	var asker = result.find('.asker');
-	asker.html('<p>Name: <a target="_blank" href=http://stackoverflow.com/users/' + question.owner.user_id + ' >' +
-													question.owner.display_name +
-												'</a>' +
-							'</p>' +
- 							'<p>Reputation: ' + question.owner.reputation + '</p>'
+	asker.html('<p>Name: <a target="_blank" href=http://stackoverflow.com/users/' + question.owner.user_id + ' >' +	question.owner.display_name + '</a>' + '</p>' +	'<p>Reputation: ' + question.owner.reputation + '</p>'
 	);
 
 	return result;
 };
 
+// ADDED BY MARS
+var showAnswerer = function(answerer) {
+
+	// clone our result template code
+	var result = $('.templates .answerer').clone();
+
+	// Set answerer properties in the results
+	var nameElem = result.find('.answerer-name a');
+	nameElem.attr('href', answerer.link);
+	nameElem.text(answerer.title);
+
+	var rep = result.find('.answerer-rep');
+	rep.text(answerer.reputation);
+
+	var acceptRate = result.find('.answerer-accept-rate');
+	acceptRate.text(answerer.accept_rate);
+
+	var profileImage = result.find('.answerer-profile-img');
+	profileImage.text(answerer.profile_image);
+
+	return result;
+
+};
 
 // this function takes the results object from StackOverflow
 // and creates info about search results to be appended to DOM
 var showSearchResults = function(query, resultNum) {
-	var results = resultNum + ' results for <strong>' + query;
+	var results = resultNum + ' results for <strong>' + query + "</strong>";
 	return results;
 };
 
@@ -59,27 +78,29 @@ var showError = function(error){
 // takes a string of semi-colon separated tags to be searched
 // for on StackOverflow
 var getUnanswered = function(tags) {
-	
+
 	// the parameters we need to pass in our request to StackOverflow's API
-	var request = {tagged: tags,
-								site: 'stackoverflow',
-								order: 'desc',
-								sort: 'creation'};
-	
+	var request = {
+					tagged: tags,
+					site: 'stackoverflow',
+					order: 'desc',
+					sort: 'creation'
+				};
+
 	var result = $.ajax({
-		url: "http://api.stackexchange.com/2.2/questions/unanswered",
-		data: request,
-		dataType: "jsonp",
-		type: "GET",
-		})
+					url: "http://api.stackexchange.com/2.2/questions/unanswered",
+					data: request,
+					dataType: "jsonp",
+					type: "GET",
+				})
 	.done(function(result){
 		var searchResults = showSearchResults(request.tagged, result.items.length);
 
 		$('.search-results').html(searchResults);
 
 		$.each(result.items, function(i, item) {
-			var question = showQuestion(item);
-			$('.results').append(question);
+			var answerer = showAnswerer(item);
+			$('.results').append(answerer);
 		});
 	})
 	.fail(function(jqXHR, error, errorThrown){
@@ -88,5 +109,34 @@ var getUnanswered = function(tags) {
 	});
 };
 
+//  CODE I'M ADDING TO STARTER
+var topAnswerer = function(tags) {
+	var request = {
+				tagged: tags,
+				site: 'stackoverflow',
+				period: 'all_time'
+				};
 
+	var result = $.ajax({
+					url: 'http://api.stackexchange.com/2.2/tags/{tag}/top-answerers/all_time?',
+					data: request,
+					dataType: "jsonp",
+					type: "GET",
+					})
 
+	.done(function(result){
+		var searchResults = showSearchResults(request.tagged, result.items.length);
+
+		$('.search-results').html(searchResults);
+
+		$.each(result.items, function(i, item) {
+			var answerer = showAnswerer(item);
+			$('.results').append(answerer);
+		});
+
+	})
+	.fail(function(jqXHR, error, errorThrown){
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+};
